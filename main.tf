c91b2fed2a65382db2666f92055782b1e3e21fe3
Priyanka Bedse <patole.priyaa@gmail.com>
2023-08-14 22:27:27 +0530

    pushed

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  aws_access_key_id="ASIA2PKU5HI7BDGJRXNG"
-  aws_secret_access_key="mAMhCLBcn9JkL0rNxt7PFAaf/l0FyCIltOpovSLx"
+  access_key = "ASIA2PKU5HI7BDGJRXNG"
+  secret_key = "mAMhCLBcn9JkL0rNxt7PFAaf/l0FyCIltOpovSLx"
 }
 resource "aws_s3_bucket" "bucket" {
   bucket = "test-github-action"
